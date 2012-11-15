ef07c26cd390e7801fa645df9c025d5c2eb1c92e
Sebastian Krings <sebastian@krin.gs>
2012-11-15 13:40:31 +0100

    remove analyzer shell script

--- analyzer.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sicstus -l ../codeq_analyzer.pl --goal "prolog_flag(redefine_warnings, _, off),on_exception(X,(use_module('$1'),write_clj_representation,halt),(print('{:error \"'),print(X),print('\"}'),nl,halt(1)))."
